242e3dfeae65858dbacabe4dee394157ffb1c3cc
Vishal <vishalcan@hotmail.com>
2019-10-18 18:05:49 -0700

    bool

--- src/Training/ride.java
+++ src/Training/ride.java
@@ -1,4 +1,9 @@
 package Training;
+import java.io.*;
 
 public class ride {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new FileReader("ride.in"));
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("ride.out")));
+    }
 }
