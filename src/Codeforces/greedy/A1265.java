2df510e791ef5ef69bef7faf742c5ee99e0327d7
vishal <vishalcan@hotmail.com>
2020-03-06 10:53:52 -0800

    namenum

--- src/Codeforces/greedy/A1265.java
+++ src/Codeforces/greedy/A1265.java
@@ -3,27 +3,27 @@ package Codeforces.greedy;
 import java.util.Scanner;
 
 public class A1265 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = Integer.parseInt(sc.next());
-        for (int i = 1; i <= t; ++i) {
-            String bool = sc.next();
-            char[] stream = bool.toCharArray();
-            if (!isBeaut(stream)) System.out.println("-1");
-            else {
-
-            }
-        }
-    }
-    public static boolean isBeaut(char[] arr) {
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i-1] == arr[i] && arr[i] != '?') {
-                return false;
-            }
-        }
-        return true;
-    }
-    public static char replace(char[] arr, int n) {
-
-    }
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int t = Integer.parseInt(sc.next());
+//        for (int i = 1; i <= t; ++i) {
+//            String bool = sc.next();
+//            char[] stream = bool.toCharArray();
+//            if (!isBeaut(stream)) System.out.println("-1");
+//            else {
+//
+//            }
+//        }
+//    }
+//    public static boolean isBeaut(char[] arr) {
+//        for (int i = 1; i < arr.length; i++) {
+//            if (arr[i-1] == arr[i] && arr[i] != '?') {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+//    public static char replace(char[] arr, int n) {
+//
+//    }
 }
